a7b16d7f4edd053c8e88fe397fd272fbbb6e9ceb
DeDe88&*&@ <sebastian.techera015@gmail.com>
2022-03-15 12:01:52 +0000

    Implemented CartController class.

---
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/cart"})
+public class CartController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CartDao cart = CartDaoMem.getInstance();
+        TemplateEngine templateEngine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        context.setVariable("cart", cart);
+        templateEngine.process("product/cart.html", context, resp.getWriter());
+    }
+}
